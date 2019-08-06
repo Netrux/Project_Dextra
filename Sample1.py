26aac9a1df75198fb837820d9986059254bd652e
Manik Mittal <45773405+thecalmguy@users.noreply.github.com>
2019-08-06 23:07:24 +0530

    added initial mavros integration

--- Sample1.py
+++ Sample1.py
@@ -1,26 +1,24 @@
+#!/usr/bin/env python
+import rospy
+import mavros
+from mavros.msg import State
+from mavros.srv import CommandBool, SetMode
+
 import os, sys, inspect, thread, time
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-# Windows and Linux
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
-# Mac
-#arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
-
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
-
 import Leap
 
-#def main():
-#
-#   # Keep this process running until Enter is pressed
-#    print "Press Enter to quit..."
-#    try:
-#        sys.stdin.readline()
-#    except KeyboardInterrupt:
-#        pass
+current_state = State()
+def state_cb(state):
+    global current_state
+    current_state = state
+
 class SampleListener(Leap.Listener):
 
     def on_connect(self, controller):
-        print "Connected"
+        print("Connected")
 
     def on_frame(self, controller):
         print "Frame available"
@@ -29,8 +27,8 @@ class SampleListener(Leap.Listener):
         position = hands.palm_position
         velocity = hands.palm_velocity
         direction = hands.direction
-        pitch = hands.direction.pitch
-        yaw = hands.direction.yaw
+        global pitch = hands.direction.pitch
+        global yaw = hands.direction.yaw
         roll = hands.palm_normal.roll
         strength = hands.grab_strength
         sphere_center = hands.sphere_center
@@ -71,8 +69,34 @@ def main():
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
 
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
+    rate = rospy.Rate(20.0)
+    #keep on trying to connect if not connected
+    while not current_state.connected:
+        rate.sleep()
+
+    last_request = rospy.get_rostime()
+    while not rospy.is_shutdown():
+        now = rospy.get_rostime()
+        if current_state.mode != "OFFBOARD" and (now - last_request > rospy.Duration(5.)):
+            set_mode_client(base_mode=0, custom_mode="OFFBOARD")
+            last_request = now
+        else:
+            if not current_state.armed and (now - last_request > rospy.Duration(5.)):
+               arming_client(True)
+               last_request = now
+
+        # older versions of PX4 always return success==True, so better to check Status instead
+        if prev_state.armed != current_state.armed:
+            rospy.loginfo("Vehicle armed: %r" % current_state.armed)
+        if prev_state.mode != current_state.mode:
+            rospy.loginfo("Current mode: %s" % current_state.mode)
+        prev_state = current_state
+
+        # Update timestamp and publish pose
+        pose.header.stamp = rospy.Time.now()
+        local_pos_pub.publish(pose)
+        rate.sleep()
+
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
